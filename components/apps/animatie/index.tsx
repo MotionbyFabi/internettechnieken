3de9d65a049ed1b861aebe55c6bcfcadca6bdbd3
motionbyfabi <fabiodinota@gmail.com>
2022-10-27 11:54:54 +0200

    fix

--- components/apps/animatie/index.tsx
+++ components/apps/animatie/index.tsx
@@ -3,10 +3,10 @@ import React from "react";
 const Animatie = () => {
   return (
     <div className="grid grid-cols-2 p-2 place-content-center w-full gap-2">
-      <div className="w-full h-[100px] rounded-xl bg-black"></div>
-      <div className="w-full h-[400px] rounded-xl bg-black"></div>
-      <div className="w-full h-[200px] rounded-xl bg-black"></div>
-      <div className="w-full h-[600px] rounded-xl bg-black"></div>
+      <div className="w-full h-[100px] rounded-xl "></div>
+      <div className="w-full h-[400px] rounded-xl "></div>
+      <div className="w-full h-[200px] rounded-xl "></div>
+      <div className="w-full h-[600px] rounded-xl "></div>
     </div>
   );
 };
